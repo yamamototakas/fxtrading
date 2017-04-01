c45b3b8a876a5d70a090306751d4951d89152a01
pêtty <ya.ma.mo.to.ta.ka.s@gmail.com>
2017-04-01 13:42:02 +0700

    new files

---
+++ agents/tensorflow_iris.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import urllib
+
+import tensorflow as tf
+import numpy as np
+
+IRIS_TRAINING = "iris_training.csv"
+IRIS_TRAINING_URL = "http://download.tensorflow.org/data/iris_training.csv"
+
+IRIS_TEST = "iris_test.csv"
+IRIS_TEST_URL = "http://download.tensorflow.org/data/iris_test.csv"
